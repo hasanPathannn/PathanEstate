4d0ac62783b64d267dee630bd96fe1755bd32587
hasanPathannn <hasancompetitivecoding@gmail.com>
2024-04-01 06:23:25 +0530

    Database connected

--- API/index.js
+++ API/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("DB connect"))
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server is runnig");
