31d1393e0a03551e41442a7ed3d2ad3313a25bf0
hasanPathannn <hasancompetitivecoding@gmail.com>
2024-04-07 22:37:07 +0530

    Listing Api created and createListing functionality added

---
+++ API/models/listing.model.js
@@ -0,0 +1,63 @@
+import moongose from "mongoose";
+
+const listingSchema = moongose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountedPrice: {
+      type: Number,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    imageURLS: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = moongose.model("Listing", listingSchema);
+
+export default Listing;
---
+++ API/routes/listing.route.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { verifyToken } from "../utils/verifyUser.js";
+import { createListing } from "../controllers/listing.controller.js";
+const router = express(Router);
+
+router.post("/create", verifyToken, createListing);
+
+export default router;
